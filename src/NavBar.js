be3002e0a2bce3e5973c8f9d2956d478293cd9dc
Claudio Yanez <claudioyanezcab@gmail.com>
2021-08-23 16:27:35 -0400

    NavBar

---
+++ src/NavBar.js
@@ -0,0 +1,18 @@
+
+import './App.css';
+import NavBar from './components/NavBar/NavBar';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar />
+      <h1>Tienda E-Commerce de Claudio Yáñez</h1>
+
+      
+    </div>
+
+  );
+}
+
+export default App;
